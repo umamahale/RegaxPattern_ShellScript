74285b9b2066a7149bdbfb20ec09134c6b329d56
umamahale <umagasmos@gmail.com>
2021-10-09 08:44:25 -0400

    first message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RegexOfPatternMatchingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phone;
+            bool result;
+            ValidatePhoneNumber validatePhoneNumber = new ValidatePhoneNumber();
+            Console.WriteLine("enter phone number");
+            phone = Console.ReadLine();
+            result = validatePhoneNumber.PhoneNumberCheck(phone);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ ValidatePhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexOfPatternMatchingDemo
+{
+    class ValidatePhoneNumber
+    {
+        public string RegexPattern = "^[9]{1}[1]{1}[0-9]{10}&";
+        public bool PhoneNumberCheck(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, RegexPattern );
+        }
+    }
+}
