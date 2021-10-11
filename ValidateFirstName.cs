3fc6f6a47e8908827aa5ae41bb1f06a5d858e8d9
umamahale <umagasmos@gmail.com>
2021-10-11 01:25:35 -0400

    first message

---
+++ ValidateFirstName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexOfPatternMatchingDemo
+{
+    class ValidateFirstName
+    {
+        public string RegexPattern = "^[A-Z][a-ZA-Z]*$";
+        public bool PhoneNumberCheck(string firstname)
+        {
+            return Regex.IsMatch(firstname, RegexPattern);
+        }
+
+        internal static bool FirstNameCheck(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
