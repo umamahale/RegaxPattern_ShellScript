eea0c4ac3a7181337a5bd5fe0196aaeb694d5712
umamahale <umagasmos@gmail.com>
2021-10-11 01:27:23 -0400

    first message

---
+++ ValidateLastName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexOfPatternMatchingDemo
+{
+    class ValidateLastName
+    {
+
+        public string RegexPattern = "^[A-Z][a-ZA-A]*$";
+        public bool LastNameCheck(string lastname)
+        {
+            return Regex.IsMatch(lastname, RegexPattern);
+        }
+
+        internal static bool LastNameCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
